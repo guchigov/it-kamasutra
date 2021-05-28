7c501f183520d17dfba750c45bf46127c3346e8b
unknown <guchigow@yandex.kz>
2021-05-29 03:12:26 +0600

    css moduls in the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import Header from "./components/Header";
+ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
-import Profile from "./components/Profile";
+ import Profile from "./components/Profile";
 
 const App = () => {
   return (
