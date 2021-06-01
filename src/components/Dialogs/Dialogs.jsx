a0daef13b2f0de164492d279c410245a89ce590f
unknown <guchigow@yandex.kz>
2021-06-02 02:31:10 +0600

    add users dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,11 +4,49 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+
+
+            <div className={classes.usersDialogs}>
+                <div className={classes.user + ' ' + classes.activeUser}>
+                    Dimich
+                </div>
+                <div className={classes.user}>
+                    Andy
+                </div>
+                <div className={classes.user}>
+                    Victor
+                </div>
+                <div className={classes.user}>
+                    Sveta
+                </div>
+                <div className={classes.user}>
+                    Maria
+                </div>
+                <div className={classes.user}>
+                    Hannah
+                </div>
+
+
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.mesage}>
+                    Hello! how are u?
+                </div>
+
+                <div className={classes.mesage}>
+                    yo!
+                </div>
+
+                <div className={classes.mesage}>
+                    See you.
+                </div>
+            </div>
 
 
         </div>
+
     );
 
 }
