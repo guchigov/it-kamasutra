61bc1baddfce835ae9ea268e55812d5227d67d57
unknown <guchigow@yandex.kz>
2021-06-03 02:28:10 +0600

    props dialogs 1 part

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,67 +2,56 @@ import React from 'react';
 import classes from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const UserDialog = (props) => {
+    let way = "/dialogs/" + props.id;
+
+    return (
+        <div className={classes.user + ' ' + classes.activeUser}>
+            <NavLink to={way}>{props.name}</NavLink>
+        </div>
+
+
+    );
+}
+
+
+const Mesage = (props) => {
+    return (
+        <div className={classes.mesage}>
+            {props.message}
+        </div>
+    );
+}
+
 
 const Dialogs = (props) => {
     return (
 
-            <div className={classes.dialogs}>
+        <div className={classes.dialogs}>
+
+            <div className={classes.usersDialogs}>
+            <UserDialog name='Dimich' id='1'/>
+            <UserDialog name='Maria' id='2'/>
+            <UserDialog name='Hannah' id='3'/>
+            <UserDialog name='Rob' id='4'/>
+            <UserDialog name='David' id='5'/>
+            <UserDialog name='Victor' id='6'/>
 
 
-                <div className={classes.usersDialogs}>
-                    <div className={classes.user + ' ' + classes.activeUser}>
-                        <NavLink to='/dialogs/1'>
-                            Dimich
-                        </NavLink>
-                    </div>
-                    <div className={classes.user}>
-                        <NavLink to='/dialogs/2'>
-                            Andy
-                        </NavLink>
-                    </div>
-                    <div className={classes.user}>
-                        <NavLink to='/dialogs/3'>
-                            Victor
-                        </NavLink>
-                    </div>
-                    <div className={classes.user}>
-                        <NavLink to='/dialogs/4'>
-                            Sveta
-                        </NavLink>
-                    </div>
-                    <div className={classes.user}>
-                        <NavLink to='/dialogs/5'>
-                            Maria
-                        </NavLink>
-                    </div>
-                    <div className={classes.user}>
-                        <NavLink to='/dialogs/6'>
-                            Hannah
-                        </NavLink>
-                    </div>
+        </div>
+
+    <div className={classes.messages}>
+      <Mesage message='Hello' />
+      <Mesage  message='Hello World!'/>
+      <Mesage message='All love triangles' />
+      <Mesage message='nice 2mu'/>
+    </div>
 
 
-                </div>
-
-                <div className={classes.messages}>
-                    <div className={classes.mesage}>
-                        Hello! how are u?
-                    </div>
-
-                    <div className={classes.mesage}>
-                        yo!
-                    </div>
-
-                    <div className={classes.mesage}>
-                        See you.
-                    </div>
-                </div>
+</div>
 
 
-            </div>
-
-
-    );
+);
 
 }
 export default Dialogs;
