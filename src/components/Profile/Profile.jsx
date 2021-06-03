8a007ec29f521b2a3c343420cfe1abd93ae8181b
unknown <guchigow@yandex.kz>
2021-06-04 03:09:34 +0600

    css navBar profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import classes from './Profile.module.css';
+import classes from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import Disc from "./Discr/Disc";
 import Wallpaper from "./Wallpaper/Wallpaper";
@@ -8,8 +8,11 @@ const Profile = () => {
     return (
         <div>
             <Wallpaper/>
-            <Disc/>
-            <MyPosts/>
+            <div className={classes.afterWall}>
+                <Disc/>
+                <MyPosts/>
+            </div>
+
 
         </div>
 
