016b836aab67b0810924e29c42f0c657c1bd043a
unknown <guchigow@yandex.kz>
2021-06-05 03:19:09 +0600

    Dialogs props Data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -25,33 +25,53 @@ const Mesage = (props) => {
 
 
 const Dialogs = (props) => {
+    let UserDialogsData = [
+        {id: 1, name: 'Dimich'},
+        {id: 2, name: 'Maria'},
+        {id: 3, name: 'Hannah'},
+        {id: 4, name: 'Rob'},
+        {id: 5, name: 'David'},
+        {id: 6, name: 'Victor'}
+
+    ]
+    let MessagesData = [
+        {id: 1, message: "Hello"},
+        {id: 2, message: "Hello World!"},
+        {id: 3, message: "All love triangle"},
+        {id: 4, message: "Nice 2 mu"},
+        {id: 5, message: "Yo"},
+        {id: 6, message: "Should Lern React Fast"}
+
+    ]
     return (
 
         <div className={classes.dialogs}>
 
             <div className={classes.usersDialogs}>
-            <UserDialog name='Dimich' id='1'/>
-            <UserDialog name='Maria' id='2'/>
-            <UserDialog name='Hannah' id='3'/>
-            <UserDialog name='Rob' id='4'/>
-            <UserDialog name='David' id='5'/>
-            <UserDialog name='Victor' id='6'/>
+                <UserDialog name={UserDialogsData[0].name} id={UserDialogsData[0].id}/>
+                <UserDialog name={UserDialogsData[1].name} id={UserDialogsData[1].id}/>
+                <UserDialog name={UserDialogsData[2].name} id={UserDialogsData[2].id}/>
+                <UserDialog name={UserDialogsData[3].name} id={UserDialogsData[3].id}/>
+                <UserDialog name={UserDialogsData[4].name} id={UserDialogsData[4].id}/>
+                <UserDialog name={UserDialogsData[5].name} id={UserDialogsData[5].id}/>
+
+
+            </div>
+
+            <div className={classes.messages}>
+                <Mesage message={MessagesData[0].message}/>
+                <Mesage message={MessagesData[1].message}/>
+                <Mesage message={MessagesData[2].message}/>
+                <Mesage message={MessagesData[3].message}/>
+                <Mesage message={MessagesData[4].message}/>
+                <Mesage message={MessagesData[5].message}/>
+            </div>
 
 
         </div>
 
-    <div className={classes.messages}>
-      <Mesage message='Hello' />
-      <Mesage  message='Hello World!'/>
-      <Mesage message='All love triangles' />
-      <Mesage message='nice 2mu'/>
-    </div>
 
-
-</div>
-
-
-);
+    );
 
 }
 export default Dialogs;
