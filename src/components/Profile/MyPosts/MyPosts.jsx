8a458f91f32bccf856d40bb38d66eb91d96e5a3d
unknown <guchigow@yandex.kz>
2021-06-05 04:00:52 +0600

    MypostsData props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let PostsData = [
+        {id: 1, message: 'How are you ?', likescount: 3},
+        {id: 2, message: 'We will rock U', likescount: 10},
+        {id: 3, message: 'Check my Git', likescount: 0},
+        {id: 4, message: 'React is perfect', likescount: 100}
+    ]
     return (
         <div className={classes.mypost}>
             <div>
@@ -16,10 +22,10 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.divPost}>
-                <Post message='How are you?' likescount='3'/>
-                <Post message='I learn React' likescount='10'/>
-                <Post message='Hello' likescount='0'/>
-                <Post message='I use react' likescount='100'/>
+                <Post message={PostsData[0].message} likescount={PostsData[0].likescount}/>
+                <Post message={PostsData[1].message} likescount={PostsData[1].likescount}/>
+                <Post message={PostsData[2].message} likescount={PostsData[2].likescount}/>
+                <Post message={PostsData[3].message} likescount={PostsData[3].likescount}/>
             </div>
         </div>
 
