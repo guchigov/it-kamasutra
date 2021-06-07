c539048aee6c88deded01796552a1c15be8cfa2b
unknown <guchigow@yandex.kz>
2021-06-08 01:19:13 +0600

    props in indexJS

--- src/App.js
+++ src/App.js
@@ -10,35 +10,10 @@ import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 
-const App = () => {
-    let PostsData = [
-        {id: 1, message: 'How are you ?', likescount: 3},
-        {id: 2, message: 'We will rock U', likescount: 10},
-        {id: 3, message: 'Check my Git', likescount: 0},
-        {id: 3, message: 'Check my Git', likescount: 0},
-        {id: 3, message: 'Check my Git', likescount: 0},
-        {id: 3, message: 'Check my Git', likescount: 0},
-        {id: 3, message: 'Check my Git', likescount: 0},
-        {id: 4, message: 'React is perfect', likescount: 100}
-    ]
-
-    let UserDialogsData = [
-        {id: 1, name: 'Dimich'},
-        {id: 2, name: 'Maria'},
-        {id: 3, name: 'Hannah'},
-        {id: 4, name: 'Rob'},
-        {id: 5, name: 'David'},
-        {id: 6, name: 'Victor'}
-
-    ]
-    let MessagesData = [
-        {id: 1, message: "Hello"},
-        {id: 2, message: "Hello World!"},
-        {id: 3, message: "All love triangle"},
-        {id: 4, message: "Nice 2 mu"},
-        {id: 5, message: "Yo"},
-        {id: 6, message: "Should Lern React Fast"}
-    ]
+const App = (props) => {
+    let UserDialogsData = props.UserDialogsDataProps;
+    let MessagesData = props.MessagesDataProps;
+    let PostsData = props.PostsDataProps;
     return (
         <BrowserRouter>
         <div className='app-wrapper'>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+let PostsData = [
+    {id: 1, message: 'How are you ?', likescount: 3},
+    {id: 2, message: 'We will rock U', likescount: 10},
+    {id: 3, message: 'Check my Git', likescount: 0},
+    {id: 3, message: 'Check my Git', likescount: 0},
+    {id: 3, message: 'Check my Git', likescount: 0},
+    {id: 3, message: 'Check my Git', likescount: 0},
+    {id: 3, message: 'Check my Git', likescount: 0},
+    {id: 4, message: 'React is perfect', likescount: 100}
+]
 
+let UserDialogsData = [
+    {id: 1, name: 'Dimich'},
+    {id: 2, name: 'Maria'},
+    {id: 3, name: 'Hannah'},
+    {id: 4, name: 'Rob'},
+    {id: 5, name: 'David'},
+    {id: 6, name: 'Victor'}
+
+]
+let MessagesData = [
+    {id: 1, message: "Hello"},
+    {id: 2, message: "Hello World!"},
+    {id: 3, message: "All love triangle"},
+    {id: 4, message: "Nice 2 mu"},
+    {id: 5, message: "Yo"},
+    {id: 6, message: "Should Lern React Fast"}
+]
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App PostsDataProps={PostsData} UserDialogsDataProps={UserDialogsData} MessagesDataProps={MessagesData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
