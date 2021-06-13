6b38be15d0233abb02de389e752d649c35b856f0
unknown <guchigow@yandex.kz>
2021-06-13 19:40:18 +0600

    less dilogs

--- src/redux/state.js
+++ src/redux/state.js
@@ -17,24 +17,7 @@ let state = {
             {id: 3, name: 'Hannah', avatar: 'https://cs50.harvard.edu/college/2019/fall/staff/media/Emma-Humphrey.png'},
             {id: 4, name: 'Rob', avatar: 'https://cra.org/wp-content/uploads/2018/05/Robert-Bowden.jpg'},
             {id: 5, name: 'David', avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/David_J._Malan.png/220px-David_J._Malan.png'},
-            {id: 6, name: 'Dimich', avatar: 'https://i.ytimg.com/vi/iJ4UGNGzJzY/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBnmgSojZdsUkxXfQLs7wK5dW0irw'},
-            {id: 7, name: 'Maria', avatar: 'https://www.seas.harvard.edu/sites/default/files/styles/embedded_image_large/public/images/news/zlatkova_maria_web_square.jpg?itok=Z2IqHSei'},
-            {id: 8, name: 'Hannah', avatar: 'https://cs50.harvard.edu/college/2019/fall/staff/media/Emma-Humphrey.png'},
-            {id: 9, name: 'Rob', avatar: 'https://cra.org/wp-content/uploads/2018/05/Robert-Bowden.jpg'},
-            {id: 10, name: 'David', avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/David_J._Malan.png/220px-David_J._Malan.png'},
-            {id: 11, name: 'Victor', avatar: 'https://i.ytimg.com/an/TW0FUhT0m-Bqg2trTbSs0g/featured_channel.jpg?v=5fc22bf3'},
-            {id: 12, name: 'Maria', avatar: 'https://www.seas.harvard.edu/sites/default/files/styles/embedded_image_large/public/images/news/zlatkova_maria_web_square.jpg?itok=Z2IqHSei'},
-            {id: 13, name: 'Hannah', avatar: 'https://cs50.harvard.edu/college/2019/fall/staff/media/Emma-Humphrey.png'},
-            {id: 14, name: 'Rob', avatar: 'https://cra.org/wp-content/uploads/2018/05/Robert-Bowden.jpg'},
-            {id: 15, name: 'David', avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/David_J._Malan.png/220px-David_J._Malan.png'},
-            {id: 16, name: 'Dimich', avatar: 'https://i.ytimg.com/vi/iJ4UGNGzJzY/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBnmgSojZdsUkxXfQLs7wK5dW0irw'},
-            {id: 17, name: 'Maria', avatar: 'https://www.seas.harvard.edu/sites/default/files/styles/embedded_image_large/public/images/news/zlatkova_maria_web_square.jpg?itok=Z2IqHSei'},
-            {id: 18, name: 'Hannah', avatar: 'https://cs50.harvard.edu/college/2019/fall/staff/media/Emma-Humphrey.png'},
-            {id: 19, name: 'Rob', avatar: 'https://cra.org/wp-content/uploads/2018/05/Robert-Bowden.jpg'},
-            {id: 20, name: 'David', avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/David_J._Malan.png/220px-David_J._Malan.png'},
-            {id: 21, name: 'Victor', avatar: 'https://i.ytimg.com/an/TW0FUhT0m-Bqg2trTbSs0g/featured_channel.jpg?v=5fc22bf3'}
-
-
+            {id: 6, name: 'Victor', avatar: 'https://i.ytimg.com/an/TW0FUhT0m-Bqg2trTbSs0g/featured_channel.jpg?v=5fc22bf3'}
         ],
         messageState: [
             {id: 1, message: "Hello"},
@@ -43,21 +26,6 @@ let state = {
             {id: 4, message: "Nice 2 mu"},
             {id: 5, message: "Yo"},
             {id: 6, message: "Hello"},
-            {id: 7, message: "Hello World!"},
-            {id: 8, message: "All love triangle"},
-            {id: 9, message: "Nice 2 mu"},
-            {id: 10, message: "Yo"},
-            {id: 11, message: "Hello"},
-            {id: 12, message: "Hello World!"},
-            {id: 13, message: "All love triangle"},
-            {id: 14, message: "Nice 2 mu"},
-            {id: 15, message: "Yo"},
-            {id: 16, message: "Hello"},
-            {id: 17, message: "Hello World!"},
-            {id: 18, message: "All love triangle"},
-            {id: 19, message: "Nice 2 mu"},
-            {id: 20, message: "Yo"},
-            {id: 21, message: "Should Lern React Fast"}
         ]
     },
     navBar: [
