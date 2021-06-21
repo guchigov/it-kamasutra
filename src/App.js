4df36291e8530a8c10515c6219212fb5070cad84
unknown <guchigow@yandex.kz>
2021-06-22 01:28:46 +0600

    BrowserRouter in indexjs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -15,7 +15,7 @@ import Settings from "./components/Settings/Settings";
 const App = (props) => {
 
     return (
-        <BrowserRouter>
+
             <div className='app-wrapper'>
 
                 <Header/>
@@ -36,7 +36,7 @@ const App = (props) => {
 
 
             </div>
-        </BrowserRouter>
+
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import store from "./redux/state";
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
 
 let rerender = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={store.getState()} dispatch={store.dispatch.bind(store)}/>
+            <BrowserRouter>
+            <App state={state} dispatch={store.dispatch.bind(store)}/>
+            </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
